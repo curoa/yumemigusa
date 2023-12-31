dbccd0839ff4ac0dd3775181bdd9255d75c97abe
curoa <yuuseitori@gmail.com>
2023-12-31 18:16:10 +0900

    add memo

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,10 +8,18 @@ require "treesitter"
 
 vim.cmd('source ~/.config/nvim/_init.vim')
 
+-- MEMO
+-- below are useful
+-- :scriptnames
+-- :TSHighlightCapturesUnderCursor
+-- :SyntaxInfo
+
 -- TODO
 -- TODO etc highlight
 -- highlight f"{fuga} hoge"
--- maybe useful: https://neovim.io/doc/user/syntax.html
+-- ref.
+-- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#adding-queries
+-- /home/blackcat/.local/share/nvim/site/pack/packer/start/nvim-treesitter/queries/python
 
 
 -- TODO
