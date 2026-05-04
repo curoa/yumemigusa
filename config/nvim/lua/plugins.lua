2f729ba76bf47e3d6c17a0262062bea1f3fdae18
curoa <yuuseitori@gmail.com>
2026-05-04 13:34:36 +0900

    update nvim surround setting

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -20,25 +20,48 @@ require("packer").startup(function()
 		'nvim-treesitter/playground', opt=true, cmd={"TSHighlightCapturesUnderCursor"}
 	})
 	use({
+		-- "kylechui/nvim-surround",
+		-- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		-- config = function()
+		-- 	require("nvim-surround").setup({
+		-- 		-- Configuration here, or leave empty to use defaults
+		-- 		keymaps = {
+		-- 			insert = "<C-z>s", -- default <c-g> is conflict to my map
+		-- 			insert_line = "<C-z>S", -- default <c-g> is conflict to my map
+		-- 			normal = "ys",
+		-- 			normal_cur = "yss",
+		-- 			normal_line = "yS",
+		-- 			normal_cur_line = "ySS",
+		-- 			visual = "S",
+		-- 			visual_line = "gS",
+		-- 			delete = "ds",
+		-- 			change = "cs",
+		-- 			change_line = "cS",
+		-- 		},
+		-- 	})
+		-- end
 		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		tag = "*",
 		config = function()
+			-- stop default mappting
+			vim.g.nvim_surround_no_insert_mappings = true
+			-- vim.g.nvim_surround_no_visual_mappings = true
+			-- vim.g.nvim_surround_no_normal_mappings = true
 			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-				keymaps = {
-					insert = "<C-z>s", -- default <c-g> is conflict to my map
-					insert_line = "<C-z>S", -- default <c-g> is conflict to my map
-					normal = "ys",
-					normal_cur = "yss",
-					normal_line = "yS",
-					normal_cur_line = "ySS",
-					visual = "S",
-					visual_line = "gS",
-					delete = "ds",
-					change = "cs",
-					change_line = "cS",
-				},
+				-- pass
 			})
+			local keymap = vim.keymap.set
+			-- Insert mode (C-z s / C-z S)
+			keymap("i", "<C-z>s", "<Plug>(nvim-surround-insert)", { desc = "Add a surrounding pair (insert mode)" })
+			keymap("i", "<C-z>S", "<Plug>(nvim-surround-insert-line)", { desc = "Add a surrounding pair on a new line (insert mode)" })
+			-- below memo is default
+			-- keymap("n", "ys", "<Plug>(nvim-surround-normal)")
+			-- keymap("n", "yss", "<Plug>(nvim-surround-normal-cur)")
+			-- keymap("n", "yS", "<Plug>(nvim-surround-normal-line)")
+			-- keymap("n", "ySS", "<Plug>(nvim-surround-normal-cur-line)")
+			-- keymap("n", "ds", "<Plug>(nvim-surround-delete)")
+			-- keymap("n", "cs", "<Plug>(nvim-surround-change)")
+			-- Visual mode (S / gS)
 		end
 	})
 end)
